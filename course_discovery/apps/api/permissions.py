9152d64f5727825982a42d4a35eccab54b23bb32
Muhammad Haseeb <muhammad.haseeb@edly.io>
2021-02-17 14:36:39 +0500

    Adding logging in permissions

--- course_discovery/apps/api/permissions.py
+++ course_discovery/apps/api/permissions.py
@@ -1,4 +1,7 @@
+import logging
 from rest_framework.permissions import BasePermission
+
+log = logging.getLogger(__name__)
 
 
 class ReadOnlyByPublisherUser(BasePermission):
@@ -6,6 +9,8 @@ class ReadOnlyByPublisherUser(BasePermission):
     Custom Permission class to check user is a publisher user.
     """
     def has_permission(self, request, view):
+        log.info(request.user)
+        log.info(request.user.groups)
         if request.method == 'GET':
                 return request.user.groups.exists()
         return True
