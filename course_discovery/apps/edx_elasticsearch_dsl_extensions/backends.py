55b333f879a43e77076333527c19be927027ac1a
waleed.mujahid <waleed.mujahid@arbisoft.com>
2025-09-03 16:50:25 +0500

    feat: add support for multiple comma separated values in facets

--- course_discovery/apps/edx_elasticsearch_dsl_extensions/backends.py
+++ course_discovery/apps/edx_elasticsearch_dsl_extensions/backends.py
@@ -123,7 +123,8 @@ class FacetedFieldSearchFilterBackend(FacetedSearchFilterBackend):
 
             # Use the actual elasticsearch field name from configuration, not the facet key
             elasticsearch_field = field_facets[field]['field']
-            _filters.append(ESDSLQ('term', **{elasticsearch_field: value}))
+            values = [v.strip() for v in value.split(',') if v.strip()]
+            _filters.append(ESDSLQ('terms', **{elasticsearch_field: values}))
 
         queryset = queryset.query('bool', **{'filter': _filters})
         return queryset
