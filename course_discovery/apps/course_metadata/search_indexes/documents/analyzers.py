e04790e6156c3c7973faa741baef1a111496c276
waleed.mujahid <waleed.mujahid@arbisoft.com>
2025-09-05 16:34:42 +0500

    feat: support one character searching

--- course_discovery/apps/course_metadata/search_indexes/documents/analyzers.py
+++ course_discovery/apps/course_metadata/search_indexes/documents/analyzers.py
@@ -26,7 +26,7 @@ synonym_text = analyzer(
 edge_ngram_completion_filter = token_filter(
     'edge_ngram_completion_filter',
     type="edge_ngram",
-    min_gram=2,
+    min_gram=1,
     max_gram=22
 )
 
